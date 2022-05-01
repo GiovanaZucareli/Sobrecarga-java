94551c0c9817552dcc84a49ed14155accda8d464
Giovana Zucareli <72563768+GiovanaZucareli@users.noreply.github.com>
2022-05-01 14:15:13 -0300

    Create chamadaAreas.java

---
+++ chamadaAreas.java
@@ -0,0 +1,7 @@
+public class chamadaAreas{
+        public static void main(String args[]){
+               areaQuadrado obj = new areaQuadrado();
+               System.out.println(obj.areaT(2,3));
+               System.out.println(obj.areaQ(2,2));
+        }
+}
